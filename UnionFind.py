494e98f5dabc555f7a11ebc0c3bc8b9f84969c8b
huskyhehe <zhou.he1@northeastern.edu>
2023-04-30 16:19:45 -0700

    Add UnionFind

---
+++ UnionFind.py
@@ -0,0 +1,44 @@
+class UnionFind:
+    def __init__(self, size: int):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x: int) -> int:
+        if x == self.root[x]:
+            return x
+        return self.find(self.root[x])
+
+    def union(self, x: int, y: int) -> None:
+        rx = self.find(x)
+        ry = self.find(y)
+        if rx == ry:
+            return
+        if self.rank[rx] >= self.rank[ry]:
+            self.root[ry] = rx
+            rank_up = 1 if self.rank[rx] == self.rank[ry] else 0
+            self.rank[rx] += rank_up
+        else:
+            self.root[rx] = ry
+
+    def connected(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+
+def main():
+    uf = UnionFind(10)
+    # 1 - 2 - 5 - 6 - 7  3 - 8 - 9  4
+    uf.union(1, 2)
+    uf.union(2, 5)
+    uf.union(5, 6)
+    uf.union(6, 7)
+    uf.union(3, 8)
+    uf.union(8, 9)
+    print(uf.connected(1, 5))  # True
+    print(uf.connected(4, 9))  # False
+
+    uf.union(4, 9)
+    print(uf.connected(4, 9))  # True
+
+
+if __name__ == "__main__":
+    main()
